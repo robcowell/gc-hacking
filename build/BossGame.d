20696d05f1320facc8e89b7a2f8d561dd96c2be9
Rob Cowell <robcowell@klivian.com>
2022-05-04 20:13:26 +0100

    Initial commit. My paths are not your paths, be warned

---
+++ build/BossGame.d
@@ -0,0 +1,121 @@
+BossGame.o: D:/code/gcdev/source/BossGame.c \
+ D:/devkitPro/libogc/include/gccore.h \
+ D:/devkitPro/libogc/include/ogc/dsp.h \
+ D:/devkitPro/libogc/include/gctypes.h \
+ D:/devkitPro/libogc/include/ogc/aram.h \
+ D:/devkitPro/libogc/include/gcbool.h \
+ D:/devkitPro/libogc/include/ogc/arqueue.h \
+ D:/devkitPro/libogc/include/ogc/lwp_queue.h \
+ D:/devkitPro/libogc/include/ogc/aram.h \
+ D:/devkitPro/libogc/include/ogc/arqmgr.h \
+ D:/devkitPro/libogc/include/ogc/audio.h \
+ D:/devkitPro/libogc/include/ogc/cache.h \
+ D:/devkitPro/libogc/include/ogc/card.h \
+ D:/devkitPro/libogc/include/ogc/cast.h \
+ D:/devkitPro/libogc/include/ogc/color.h \
+ D:/devkitPro/libogc/include/ogc/consol.h \
+ D:/devkitPro/libogc/include/ogc/gx_struct.h \
+ D:/devkitPro/libogc/include/ogc/dvd.h \
+ D:/devkitPro/libogc/include/ogc/disc_io.h \
+ D:/devkitPro/libogc/include/ogc/exi.h \
+ D:/devkitPro/libogc/include/ogc/gu.h \
+ D:/devkitPro/libogc/include/ogc/gx.h \
+ D:/devkitPro/libogc/include/ogc/lwp.h \
+ D:/devkitPro/libogc/include/ogc/gu.h \
+ D:/devkitPro/libogc/include/ogc/si.h \
+ D:/devkitPro/libogc/include/ogc/gx_struct.h \
+ D:/devkitPro/libogc/include/ogc/irq.h \
+ D:/devkitPro/libogc/include/ogc/context.h \
+ D:/devkitPro/libogc/include/ogc/lwp.h \
+ D:/devkitPro/libogc/include/ogc/mutex.h \
+ D:/devkitPro/libogc/include/ogc/message.h \
+ D:/devkitPro/libogc/include/ogc/semaphore.h \
+ D:/devkitPro/libogc/include/ogc/pad.h \
+ D:/devkitPro/libogc/include/ogc/tpl.h \
+ D:/devkitPro/libogc/include/ogc/gx.h \
+ D:/devkitPro/libogc/include/ogc/system.h \
+ D:/devkitPro/libogc/include/gcutil.h \
+ D:/devkitPro/libogc/include/ogc/video.h \
+ D:/devkitPro/libogc/include/ogc/video_types.h \
+ D:/devkitPro/libogc/include/ogc/usbgecko.h \
+ D:/devkitPro/libogc/include/ogc/video_types.h \
+ D:/devkitPro/libogc/include/ogc/texconv.h \
+ D:/devkitPro/libogc/include/aesndlib.h \
+ D:/devkitPro/libogc/include/gcmodplay.h \
+ D:/devkitPro/libogc/include/modplay/modplay.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB__lib.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB__inline.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_clipping.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_collision.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_fbComplex.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_fbGX.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_fbSimple.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_handle.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_pixel.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_settings.h \
+ D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_texSetup.h \
+ D:/devkitPro/libogc/include/ogc/lwp_watchdog.h \
+ D:/devkitPro/libogc/include/ogc/lwp_queue.h \
+ D:/code/gcdev/build/technique_mod.h \
+ D:/code/gcdev/build/FreeMonoBold_ttf.h
+D:/devkitPro/libogc/include/gccore.h:
+D:/devkitPro/libogc/include/ogc/dsp.h:
+D:/devkitPro/libogc/include/gctypes.h:
+D:/devkitPro/libogc/include/ogc/aram.h:
+D:/devkitPro/libogc/include/gcbool.h:
+D:/devkitPro/libogc/include/ogc/arqueue.h:
+D:/devkitPro/libogc/include/ogc/lwp_queue.h:
+D:/devkitPro/libogc/include/ogc/aram.h:
+D:/devkitPro/libogc/include/ogc/arqmgr.h:
+D:/devkitPro/libogc/include/ogc/audio.h:
+D:/devkitPro/libogc/include/ogc/cache.h:
+D:/devkitPro/libogc/include/ogc/card.h:
+D:/devkitPro/libogc/include/ogc/cast.h:
+D:/devkitPro/libogc/include/ogc/color.h:
+D:/devkitPro/libogc/include/ogc/consol.h:
+D:/devkitPro/libogc/include/ogc/gx_struct.h:
+D:/devkitPro/libogc/include/ogc/dvd.h:
+D:/devkitPro/libogc/include/ogc/disc_io.h:
+D:/devkitPro/libogc/include/ogc/exi.h:
+D:/devkitPro/libogc/include/ogc/gu.h:
+D:/devkitPro/libogc/include/ogc/gx.h:
+D:/devkitPro/libogc/include/ogc/lwp.h:
+D:/devkitPro/libogc/include/ogc/gu.h:
+D:/devkitPro/libogc/include/ogc/si.h:
+D:/devkitPro/libogc/include/ogc/gx_struct.h:
+D:/devkitPro/libogc/include/ogc/irq.h:
+D:/devkitPro/libogc/include/ogc/context.h:
+D:/devkitPro/libogc/include/ogc/lwp.h:
+D:/devkitPro/libogc/include/ogc/mutex.h:
+D:/devkitPro/libogc/include/ogc/message.h:
+D:/devkitPro/libogc/include/ogc/semaphore.h:
+D:/devkitPro/libogc/include/ogc/pad.h:
+D:/devkitPro/libogc/include/ogc/tpl.h:
+D:/devkitPro/libogc/include/ogc/gx.h:
+D:/devkitPro/libogc/include/ogc/system.h:
+D:/devkitPro/libogc/include/gcutil.h:
+D:/devkitPro/libogc/include/ogc/video.h:
+D:/devkitPro/libogc/include/ogc/video_types.h:
+D:/devkitPro/libogc/include/ogc/usbgecko.h:
+D:/devkitPro/libogc/include/ogc/video_types.h:
+D:/devkitPro/libogc/include/ogc/texconv.h:
+D:/devkitPro/libogc/include/aesndlib.h:
+D:/devkitPro/libogc/include/gcmodplay.h:
+D:/devkitPro/libogc/include/modplay/modplay.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB__lib.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB__inline.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_clipping.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_collision.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_fbComplex.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_fbGX.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_fbSimple.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_handle.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_pixel.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_settings.h:
+D:/devkitPro/portlibs/gamecube/include/grrlib/GRRLIB_texSetup.h:
+D:/devkitPro/libogc/include/ogc/lwp_watchdog.h:
+D:/devkitPro/libogc/include/ogc/lwp_queue.h:
+D:/code/gcdev/build/technique_mod.h:
+D:/code/gcdev/build/FreeMonoBold_ttf.h:
---
+++ source/BossGame.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gccore.h>
+#include <aesndlib.h>
+#include <gcmodplay.h>
+#include <grrlib.h>
+#include <ogc/lwp_watchdog.h>
+
+// include generated headers
+#include "technique_mod.h"
+#include "FreeMonoBold_ttf.h"
+
+static MODPlay play;
+static u8 CalculateFrameRate();
+
+
+//---------------------------------------------------------------------------------
+int main(int argc, char **argv) {
+//---------------------------------------------------------------------------------
+
+	char FPS[255] = "";
+	// Initialise GRRLib
+	GRRLIB_Init();
+
+	// Load the font from memory
+    GRRLIB_ttfFont *myFont = GRRLIB_LoadTTF(FreeMonoBold_ttf, FreeMonoBold_ttf_size);
+
+	 // Create an empty texture to store a copy of the screen
+    GRRLIB_texImg *CopiedImg = GRRLIB_CreateEmptyTexture(rmode->fbWidth, rmode->efbHeight);
+
+	
+
+	// Initialise the attached controllers
+	PAD_Init();
+
+	// Initialise the audio subsystem
+	AESND_Init();
+
+	
+
+	MODPlay_Init(&play);
+	MODPlay_SetMOD(&play,technique_mod);
+	MODPlay_Start(&play);
+
+	// To have a cool effect anti-aliasing is turned on
+    GRRLIB_Settings.antialias = true;
+
+    // Black background
+    GRRLIB_SetBackgroundColour(0x00, 0x00, 0x00, 0xFF);
+
+	while(1) {
+	
+		GRRLIB_DrawImg(0, 0, CopiedImg, 0, 1, 1, 0xFFFFFFFF);
+		sprintf(FPS, "Current FPS: %d", CalculateFrameRate());
+            GRRLIB_PrintfTTF(500+1, 25+1, myFont, FPS, 12, 0x000000FF);
+            GRRLIB_PrintfTTF(500, 25, myFont, FPS, 12, 0xFFFFFFFF);
+		
+		
+
+		PAD_ScanPads();
+
+		int buttonsDown = PAD_ButtonsDown(0);
+
+		if( buttonsDown & PAD_BUTTON_A ) {
+			printf("Button A pressed.\n");
+		}
+
+		if (buttonsDown & PAD_BUTTON_START) {
+			exit(0);
+		}
+
+		GRRLIB_Render();
+	}
+
+	GRRLIB_FreeTTF(myFont);
+	GRRLIB_Exit();
+	exit(0);
+}
+
+/**
+ * This function calculates the number of frames we render each second.
+ * @return The number of frames per second.
+ */
+static u8 CalculateFrameRate()
+{
+    static u8 frameCount = 0;
+    static u32 lastTime;
+    static u8 FPS = 0;
+    u32 currentTime = ticks_to_millisecs(gettime());
+
+    frameCount++;
+    if(currentTime - lastTime > 1000) {
+        lastTime = currentTime;
+        FPS = frameCount;
+        frameCount = 0;
+    }
+    return FPS;
+}
